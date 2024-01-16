60e0dfb00dd0c1f64e469c7a0a54e3f9749ec00a
Juhar <jcsong06@gmail.com>
2024-01-16 17:14:39 -0600

    resetEncoders() fixed

--- src/main/java/frc/robot/swerve/SwerveModule.java
+++ src/main/java/frc/robot/swerve/SwerveModule.java
@@ -70,26 +70,26 @@ public class SwerveModule implements Sendable {
 
 
         // refreshed TalonFX rotor position signal // position value that we just refreshed in rotations
+        // return m_driveMotor.getRotorPosition().getValue() * Constants.Conversions.DriveRotToMeters;
         return m_driveMotor.getPosition().getValue() * Constants.Conversions.DriveRotToMeters;
     }
 
     // returns the position of the steering motor radians
     public double getTurningPosition() {
-        // TODO: change ticks to rotation
-        m_steerMotor.getPosition(); // TODO: WHATT?
+        // return m_steerMotor.getRotorPosition().getValue() * Constants.Conversions.SteerRotToRads;
         return m_steerMotor.getPosition().getValue() * Constants.Conversions.SteerRotToRads;
     }
 
     // gets the velocity of the drive motor in m/s
     public double getDriveVelocity() {
+        // return m_driveMotor.getRotorVelocity().getValue() * Constants.Conversions.DriveRotToMetersPerSecond;
         return m_driveMotor.getVelocity().getValue() * Constants.Conversions.DriveRotToMetersPerSecond;
-        // TODO or driveMotor.getVelocity(); and adjust ticks to rotations
     }
 
     // gets the speed at which the steering motor turns in radians per second
     public double getTurningVelocity() {
+        // return m_steerMotor.getRotorVelocity().getValue() * Constants.Conversions.SteerRotToRadsPerSecond;
         return m_steerMotor.getVelocity().getValue() * Constants.Conversions.SteerRotToRadsPerSecond;
-        //TODO or getVelocity()
     }
 
     // gets the position of the steering wheel according to the absolute encoders
@@ -120,7 +120,7 @@ public class SwerveModule implements Sendable {
      * Resets the relative encoders according the absolute encoder involving the offset
      */
     public void resetEncoders() {
-        m_steerMotor.setPosition((getAbsoluteEncoderRad()) - absoluteEncoderOffset / Constants.Conversions.SteerRotToRads);
+        m_steerMotor.setPosition((getAbsoluteEncoderRad() - absoluteEncoderOffset) / Constants.Conversions.SteerRotToRads);
         // m_driveMotor.setSelectedSensorPosition(0);
     }
 
