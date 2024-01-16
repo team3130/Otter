f9e2990881006dbf1c7ce4c71128e53126a7886b
Juhar <jcsong06@gmail.com>
2024-01-16 15:54:01 -0600

    setVoltage() instead of setControl()

--- src/main/java/frc/robot/swerve/SwerveModule.java
+++ src/main/java/frc/robot/swerve/SwerveModule.java
@@ -165,9 +165,10 @@ public class SwerveModule implements Sendable {
         state = SwerveModuleState.optimize(state, getState().angle);
         // percent output of the drive motor that the swerve controller wants you to go to by the physical max speed the bot can travel
         // TODO: underneath set control voltage output is not real
-        m_driveMotor.setControl(driveMotorVoltRequest.withOutput(12d* (state.speedMetersPerSecond / Constants.Swerve.kPhysicalMaxSpeedMetersPerSecond)));
+        // m_driveMotor.setControl(driveMotorVoltRequest.withOutput(12d* (state.speedMetersPerSecond / Constants.Swerve.kPhysicalMaxSpeedMetersPerSecond)));
+        m_driveMotor.setVoltage((12d* (state.speedMetersPerSecond / Constants.Swerve.kPhysicalMaxSpeedMetersPerSecond)));
         // set the steering motor based off the output of the PID controller
-        m_steerMotor.set(turningPidController.calculate(Math.IEEEremainder(getTurningPosition(), Math.PI * 2), state.angle.getRadians()));
+        m_steerMotor.setVoltage(12d * turningPidController.calculate(Math.IEEEremainder(getTurningPosition(), Math.PI * 2), state.angle.getRadians()));
     }
 
     /**
@@ -175,7 +176,7 @@ public class SwerveModule implements Sendable {
      * @param setpoint in radians
      */
     public void turnToAngle(double setpoint) {
-        m_steerMotor.set(turningPidController.calculate(Math.IEEEremainder(getTurningPosition(), Math.PI * 2), setpoint));
+        m_steerMotor.setVoltage(12d * turningPidController.calculate(Math.IEEEremainder(getTurningPosition(), Math.PI * 2), setpoint));
     }
 
     /**
