2e8c80e2bab9f117f92d69b49e76e371c9c83584
Juhar <jcsong06@gmail.com>
2024-01-21 03:04:21 -0600

    nothing deep

---
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -0,0 +1,94 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LEDSubsystem extends SubsystemBase {
+  private final int ledLength = 61;
+  AddressableLED led;
+  AddressableLEDBuffer ledBuffer;
+
+  /** Creates a new ExampleSubsystem. */
+  public LEDSubsystem() {
+    /// PWM port 9
+    // Must be a PWM header, not MXP or DIO
+    led = new AddressableLED(0);
+
+    // Reuse buffer
+    // Default to a length of 60, start empty output
+    // Length is expensive to set, so only set it once, then just update data
+    ledBuffer = new AddressableLEDBuffer(ledLength);
+    led.setLength(ledBuffer.getLength());
+
+    // Set the data
+    led.setData(ledBuffer);
+    led.start();
+  }
+
+
+  public void oneColor() {
+    for (var i = 0; i < ledBuffer.getLength(); i++) {
+      // Sets the specified LED to the RGB values for red
+      ledBuffer.setHSV(i, 0, 0, 128);
+    }
+    led.setData(ledBuffer);
+  }
+
+  public void rainbow() {
+    int rainbowFirstPixelHue = 20;
+    // For every pixel
+    for (var i = 0; i < ledBuffer.getLength(); i++) {
+      // Calculate the hue - hue is easier for rainbows because the color
+      // shape is a circle so only one value needs to precess
+      final var hue = (rainbowFirstPixelHue + (i * 180 / ledBuffer.getLength())) % 180;
+      // Set the value, s == saturation, v == brightness value
+      ledBuffer.setHSV(i, hue, 255, 128);
+    }
+    // Increase by to make the rainbow "move"
+    rainbowFirstPixelHue += 3;
+    // Check bounds
+    rainbowFirstPixelHue %= 180;
+    led.setData(ledBuffer);
+  }
+
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public Command exampleMethodCommand() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
