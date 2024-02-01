b0ea447661840383df4457831da3c2df4a550e8c
Coriander <145623681+CorianderSeed@users.noreply.github.com>
2024-01-31 19:08:37 -0600

    Added timer for Outtake. USE THIS INSTEAD uwu

--- src/main/java/frc/robot/commands/Amp/AmpOuttake.java
+++ src/main/java/frc/robot/commands/Amp/AmpOuttake.java
@@ -6,11 +6,13 @@ package frc.robot.commands.Amp;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Amp;
+import java.util.Timer;
 
 /** An example command that uses an example subsystem. */
 public class AmpOuttake extends Command {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
   private final Amp amp;
+  private final Timer ampTimer;
 
   /**
    * Creates a new ExampleCommand.
@@ -20,19 +22,34 @@ public class AmpOuttake extends Command {
    */
   public AmpOuttake(Amp amp) {
     this.amp = amp;
+    ampTimer = new Timer();
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(amp);
   }
 
   // Called when the command is initially scheduled.
+  /**
+  *
+   */
   @Override
   public void initialize() {
-    amp.outtakeAmp();
+    amp.primeAmp();
+    ampTimer.reset();
+    ampTimer.start();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
+
+  /**
+   * Change the hasElapsed second number to however long it takes
+   * amp to prime again
+   */
   @Override
   public void execute() {
+    if (ampTimer.hasElapsed(1)) {
+      amp.outtakeAmp();
+      ampTimer.stop();
+    }
     if (!amp.getLimitSwitch()) {
       amp.motorStop();
     }
