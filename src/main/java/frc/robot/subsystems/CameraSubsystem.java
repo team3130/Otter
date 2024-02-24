95eefff0a649b9e411bd8d48ec3094ac9a975fdf
Juhar <jcsong06@gmail.com>
2024-02-24 11:02:20 -0600

    me when slay

--- src/main/java/frc/robot/subsystems/CameraSubsystem.java
+++ src/main/java/frc/robot/subsystems/CameraSubsystem.java
@@ -23,13 +23,9 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 public class CameraSubsystem extends SubsystemBase {
-  protected PhotonCamera camera = new PhotonCamera("cam");
-  private static final ShuffleboardTab tab = Shuffleboard.getTab("PhotonCamera");
   AprilTagFieldLayout aprilTagFieldLayout;
-
   Pose2d targetPose = new Pose2d(16.58, 5.55, Rotation2d.fromRadians(0));
   Transform2d cameraToRobot = new Transform2d(3, 0, Rotation2d.fromRadians(0));
-  private PhotonTrackedTarget CorrectTarget =new PhotonTrackedTarget(0,0,0, 0, -1, new Transform3d(), new Transform3d(), 0, new ArrayList<>(), new ArrayList<>());
   private double previousPipelineTimestamp = 0;
   private int redSpeakerTargetFiducialID;
   private int blueSpeakerTargetFiducialID;
@@ -44,26 +40,21 @@ public class CameraSubsystem extends SubsystemBase {
 
   private double XtargetV = 0.2;
   private double YtargetF = 0.8;
+  private Chassis chassis;
 
 
   /**
    * Constructs a new Limelight object.
    * The limelight object will be full of null values if Constants.useAprilTags is false.
    */
-  public CameraSubsystem() {
-    ShuffleboardTab tab = Shuffleboard.getTab("Vision");
-    // Shuffleboard.getTab("Camerapls").add("target", target);
-    tab.addBoolean("hasTarget", this::hasTarget);
-    tab.addDouble("Target Yaw", this::getTargetYaw);
+  public CameraSubsystem(Chassis chassiss) {
+    chassis = chassiss;
 
     // SuppliedValueWidget<Double> targetYaw = tab.addDouble("Target Yaw", this::getTargetYaw);
     // tab.addDouble("Target Yaw", this::getTargetYaw).withPosition(2, 0).withSize(6, 4)
 
     // hasTargetQuestion = Shuffleboard.getTab("Camerapls").add("hasTarget", hasTargets).getEntry();
     // hasTargetQuestion = Shuffleboard.getTab("Camerapls").add("targetYaw", targetYaw).getEntry();
-        redSpeakerTargetFiducialID = Constants.AprilTags.speakerTargetRedFiducialID;
-
-        blueSpeakerTargetFiducialID = Constants.AprilTags.speakerTargetBlueFiducialID;
 
 
       targetController = new PIDController(targetP, targetI, targetD);
@@ -107,9 +98,7 @@ public class CameraSubsystem extends SubsystemBase {
   public double getYTargetV() {
     return YtargetF;
   }
-  public int getFiducialID() {
-    return fiducialID;
-  }
+
 
 
   public void setTargetP(double newP){
@@ -124,75 +113,28 @@ public class CameraSubsystem extends SubsystemBase {
 
 
   public double goToTargetPower() {
-    if(hasTarget()){
-      return targetController.calculate(getTargetYaw(), 0);
-    }
-    else {
-      return 0;
-    }
+      return targetController.calculate(chassis.getRotation2d().getRadians(), Math.PI);
   }
-  public PhotonTrackedTarget getTarget() {
-    if (!hasTarget()) {
-      return null;
-    } else {
-      PhotonPipelineResult result = camera.getLatestResult();
-      return result.getBestTarget();
-    }
-  }
+
 
   public void resetTargetController() {
     targetController.reset();
-    targetController.setSetpoint(0d);
-    targetController.enableContinuousInput(-2*Math.PI,2*Math.PI);
+    targetController.setSetpoint(Math.PI);
+    targetController.enableContinuousInput(-Math.PI,Math.PI);
     targetController.setTolerance(Math.toRadians(1.0));
     targetController.setPID(targetP, targetI, targetD);
   }
 
 
-  public Pose2d findRobotPose() {
-    // cameraToRobot The position of the robot relative to the camera. If the camera was
-    // mounted 3 inches behind the "origin" (usually physical center) of the robot, this would be
-    // Transform2d(3 inches, 0 inches, 0 degrees).
-    if (!hasTarget()) {
-      return null;
-    } else {
-      PhotonPipelineResult result = camera.getLatestResult();
-      double yaw = result.getBestTarget().getYaw();
-      return PhotonUtils.estimateFieldToRobot(
-              Constants.AprilTags.CAMERA_HEIGHT_METERS, Constants.AprilTags.TARGET_HEIGHT_METERS,
-              Constants.AprilTags.CAMERA_PITCH_RADIANS, Constants.AprilTags.kTargetPitch,
-              Rotation2d.fromDegrees(yaw), Navx.getRotation(), targetPose, cameraToRobot);
-    }
-  }
 
   // AprilTagFieldLayout.getTagPose(getTarget().getFiducialId())
 
-  public boolean hasTarget() {
-    return camera.hasTargets();
-  }
   public int getCorrectTargetID(){
     return fiducialID;
   }
-  public double getTargetYaw() {
-    if (!hasTarget()) {
-      return -400.0;
-    } else {
-      if (CorrectTarget != null && CorrectTarget.getYaw() != -400.0) {
-        return Math.toRadians(CorrectTarget.getYaw());
-      }
-    }
-    return -400d;
-  }
-  public double getTargetDegrees() {
-    return Math.toDegrees(getTargetYaw());
-  }
+
 
   public void initSendable(SendableBuilder builder) {
-    builder.setSmartDashboardType("Camera");
-    builder.addBooleanProperty("hasTarget", this::hasTarget, null);
-    builder.addDoubleProperty("targetYaw", this::getTargetDegrees, null);
-    builder.addIntegerProperty("fiducial", this::getCorrectTargetID, null);
-
     builder.addDoubleProperty("target P", this::getTargetP, this::setTargetP);
     builder.addDoubleProperty("target I", this::getTargetI, this::setTargetI);
     builder.addDoubleProperty("target D", this::getTargetD, this::setTargetD);
@@ -208,19 +150,6 @@ public class CameraSubsystem extends SubsystemBase {
   // This method will be called once per scheduler run
   @Override
   public void periodic() {
-    PhotonPipelineResult result = camera.getLatestResult();
-    double resultTimestamp = result.getTimestampSeconds();
-    int i = 0;
-    while (result.getTargets().size() > i && result.getTargets() != null && hasTarget() && result.getTargets().get(i).getFiducialId() != -1) {
-      if (result.getTargets().get(i).getFiducialId() == 4 || result.getTargets().get(i).getFiducialId() == 7) {
-        fiducialID = result.getTargets().get(i).getFiducialId();
-        setCurrentTag(result.getTargets().get(i));
-      }
-      i++;
-    }
-  }
-  public void setCurrentTag(PhotonTrackedTarget target){
-    CorrectTarget = target;
   }
 
   @Override
