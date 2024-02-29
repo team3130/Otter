f023668213028f85f5f7cd80904a9984e2666e8c
Juhar <jcsong06@gmail.com>
2024-02-29 13:28:21 -0600

    Deleting RobotContainer Auto comment

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -174,17 +174,6 @@ public class RobotContainer {
     }
   }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-
-  public Command getAutonomousCommand() {
-  // An example command will be run in autonomous
-  return Autos.exampleAuto(m_exampleSubsystem);
-  }
-   */
-
   // This method defines trigger -> command mappings
   // Triggers created via the Trigger constructor
   // CommandGenericHID subclass for CommandXboxController Xbox
