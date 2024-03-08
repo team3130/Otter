710b0b7f99759d1b3df5f48340531081589a0175
Juhar <jcsong06@gmail.com>
2024-03-07 20:16:06 -0600

    velocity shoot updatePID shuffleboard debug mode

--- src/main/java/frc/robot/commands/Shooter/VelocityShoot.java
+++ src/main/java/frc/robot/commands/Shooter/VelocityShoot.java
@@ -8,6 +8,7 @@ package frc.robot.commands.Shooter;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
 import frc.robot.subsystems.Shooter;
 
 public class VelocityShoot extends Command {
@@ -28,8 +29,10 @@ public class VelocityShoot extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    if (Constants.debugMode) {
+      shooter.updatePIDValues();
+    }
     shooter.configureVelocitySlots();
-    shooter.updateVelocityPID();
     shooter.setFlywheelVelocity();
   }
 
