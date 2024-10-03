29e1ba8412e7ff2c6a1973bdf54fcbec37a12aa5
Coriander <145623681+CorianderSeed@users.noreply.github.com>
2024-10-02 19:19:58 -0500

    Made ShuffleBoard for thetalimiter
    Getter and Setter for the constant (c / magnitude = theta) so we can hopefully test and tune the constant for good control

--- src/main/java/frc/robot/commands/Chassis/ThetaLimiter.java
+++ src/main/java/frc/robot/commands/Chassis/ThetaLimiter.java
@@ -3,11 +3,15 @@ package frc.robot.commands.Chassis;
 import edu.wpi.first.math.MathSharedStore;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.util.sendable.SendableBuilder;
+import frc.robot.Constants;
 
-public class ThetaLimiter {
+import java.util.function.DoubleSupplier;
+
+public class ThetaLimiter implements Sendable {
     private double prevTime;
-    private final double magvalue = .5;
-    private final double posOmegaLimit;
+    private double posOmegaLimit;
     private Translation2d prevState;
 
     public ThetaLimiter(double limitConstant, Translation2d joyStick){
@@ -15,6 +19,7 @@ public class ThetaLimiter {
         prevState = joyStick;
         prevTime = MathSharedStore.getTimestamp();
     }
+
     public Translation2d calculate(Translation2d desiredState) {
         double magnitude = desiredState.getNorm();
         double currentTime = MathSharedStore.getTimestamp();
@@ -34,4 +39,14 @@ public class ThetaLimiter {
         prevTime = currentTime;
         return prevState;
     }
-}
+    public double getPosOmegaLimit() {
+        return posOmegaLimit;
+    }
+
+    public void setPosOmegaLimit(double posOmegaLimit) {
+        this.posOmegaLimit = posOmegaLimit;
+    }
+    public void initSendable(SendableBuilder builder) {
+        builder.setSmartDashboardType("thetaLimiter");
+        builder.addDoubleProperty("thetaLimit", this::getPosOmegaLimit, this::setPosOmegaLimit);
+        }}
